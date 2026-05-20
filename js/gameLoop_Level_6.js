b29111e1ba0a78ceaaf1aaad0d6d370add6166be
dpthibeault99 <dpthibeault@email.neit.edu>
2026-05-19 20:16:24 -0400

    60 bullets per second

--- js/gameLoop_Level_6.js
+++ js/gameLoop_Level_6.js
@@ -8,6 +8,8 @@ var timer = setInterval(animate,interval);
 var fx = .90;
 var fy = .90;
 
+var bullets = [];
+
 
 player = new gameObject();
 player.x = 200;
@@ -20,9 +22,6 @@ player2.y = 600;
 player2.color = "#ff0000";
 player2.force = 1;
 
-bullet = new gameObject();
-bullet.force = 1;
-
 
 
 function animate()
@@ -30,22 +29,19 @@ function animate()
     context.clearRect(0,0,canvas.width, canvas.height);
    
     angularMovement();
+
     player.drawTriangle();
     player2.drawLeftTriangle();
-    // point();
-    // follow();
-    shoot();
-    moveBullet();
-    // bullet.drawCircle();
 
+    shoot();
+    moveBullets();
 }
+
 function angularMovement()
 { 
-    // player2.angle +=2; // infinate spins
-
     if (w)
     {
-        var radians = player.angle * Math.PI / 180; // + Math.PI / 180;
+        var radians = player.angle * Math.PI / 180;
 
         player.ax = Math.cos(radians);
         player.ay = Math.sin(radians);
@@ -56,7 +52,7 @@ function angularMovement()
 
     if (s)
     {
-        var radians = player.angle * Math.PI / 180; // + Math.PI / 180;
+        var radians = player.angle * Math.PI / 180;
 
         player.ax = Math.cos(radians);
         player.ay = Math.sin(radians);
@@ -65,24 +61,25 @@ function angularMovement()
         player.vy += player.ay * -player.force;
     }
 
-
     if(a)
     {
-        player.angle -=2;
+        player.angle -= 2;
     }
+
     if(d)
     {
-        player.angle +=2;
+        player.angle += 2;
     }
-    player.vx *=fx
-    player.vy *=fy
 
-    
+    player.vx *= fx;
+    player.vy *= fy;
+
+
     // player2
 
-    if (up) // had to swap up/down after i drew the triangle facing left
+    if (up)
     {
-        var radians = player2.angle * Math.PI / 180; // + Math.PI / 180;
+        var radians = player2.angle * Math.PI / 180;
 
         player2.ax = Math.cos(radians);
         player2.ay = Math.sin(radians);
@@ -93,7 +90,7 @@ function angularMovement()
 
     if (down)
     {
-        var radians = player2.angle * Math.PI / 180; // + Math.PI / 180;
+        var radians = player2.angle * Math.PI / 180;
 
         player2.ax = Math.cos(radians);
         player2.ay = Math.sin(radians);
@@ -102,18 +99,18 @@ function angularMovement()
         player2.vy += player2.ay * player2.force;
     }
 
-
     if(left)
     {
-        player2.angle -=2;
+        player2.angle -= 2;
     }
+
     if(right)
     {
-        player2.angle +=2;
+        player2.angle += 2;
     }
-    player2.vx *=fx
-    player2.vy *=fy
 
+    player2.vx *= fx;
+    player2.vy *= fy;
 
     player.move();
     player2.move();
@@ -121,31 +118,30 @@ function angularMovement()
 
 function shoot()
 {
-    // player 2
     if(enter)
     {
         console.log("P2 Shoots");
 
-        var radians = (player2.angle + 180) * Math.PI / 180; // + 180 because im facing left
+        var radians = (player2.angle + 180) * Math.PI / 180;
+
         var tipX = player2.x + Math.cos(radians) * (player2.width / 2);
         var tipY = player2.y + Math.sin(radians) * (player2.width / 2);
 
-        bullet.x = tipX;
-        bullet.y = tipY;
-        bullet.width = 10;
-        bullet.height = 10;
-        bullet.color = player2.color;
+        var bullet = new gameObject(tipX, tipY, 10, 10, player2.color);
 
-        bullet.ax = Math.cos(radians);
-        bullet.ay = Math.sin(radians);
+        bullet.force = 8;
+        bullet.vx = Math.cos(radians) * bullet.force;
+        bullet.vy = Math.sin(radians) * bullet.force;
 
-        bullet.vx = bullet.ax * bullet.force;
-        bullet.vy = bullet.ay * bullet.force;
+        bullets.push(bullet);
     }
 }
 
-function moveBullet()
+function moveBullets()
 {
-    bullet.move();
-    bullet.drawCircle();
+    for(var i = 0; i < bullets.length; i++)
+    {
+        bullets[i].move();
+        bullets[i].drawCircle();
+    }
 }
